7c506c2cf014226e1de25da206d922839791b6f4
Regis Seki <regisseki@gmail.com>
2021-04-05 12:46:49 -0300

    Improve tests to Products and Categories

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,20 +1,6 @@
 require 'rails_helper'
 
 describe 'Categories Api', type: :request do
-  describe 'GET /categories' do
-    before do
-      FactoryBot.create(:category, name: 'Higiene Pessoal')
-      FactoryBot.create(:category, name: 'Alimentação')
-    end
-
-    it 'returns all categories' do
-      get '/api/v1/categories'
-
-      expect(response).to have_http_status(:success)
-      expect(JSON.parse(response.body).size).to eq(2)
-    end
-  end
-
   describe 'POST /categories' do
     it 'create a new category' do
       expect {
@@ -22,6 +8,38 @@ describe 'Categories Api', type: :request do
       }.to change { Category.count }.from(0).to(1)
 
       expect(response).to have_http_status(:created)
+      expect(response_body).to eq(
+        {
+          'id' => 1,
+          'name' => 'Higiene Pessoal'
+        }
+      )
+    end
+  end
+
+  describe 'GET /categories' do
+    before do
+      FactoryBot.create(:category, name: 'Limpeza')
+      FactoryBot.create(:category, name: 'Alimentação')
+    end
+
+    it 'returns all categories' do
+      get '/api/v1/categories'
+
+      expect(response).to have_http_status(:success)
+      expect(response_body.size).to eq(2)
+      expect(response_body).to eq(
+        [
+          {
+          'id' => 2,
+          'name' => 'Limpeza'
+          },
+          {
+            'id' => 3,
+            'name' => 'Alimentação'
+          }
+        ]
+      )
     end
   end
 
